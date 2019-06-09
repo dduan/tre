67e2ebcf8aa74eaa39a194b48dc3a48fb49ae97e
Daniel Duan <daniel@duan.ca>
2019-06-09 15:06:03 -0700

    Pass reference for path finders

--- src/path_finder.rs
+++ src/path_finder.rs
@@ -2,7 +2,7 @@ use crate::file_tree::FileType;
 use std::fs::Metadata;
 use walkdir::{DirEntry, WalkDir};
 
-fn find_all_paths(root: String) -> Vec<(String, Metadata)> {
+fn find_all_paths(root: &String) -> Vec<(String, Metadata)> {
     let mut result: Vec<(String, Metadata)> = Vec::new();
     for entry in WalkDir::new(root).into_iter().filter_map(|e| e.ok()) {
         if let Ok(meta) = entry.metadata() {
@@ -19,7 +19,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn find_non_hidden_paths(root: String) -> Vec<(String, Metadata)> {
+fn find_non_hidden_paths(root: &String) -> Vec<(String, Metadata)> {
     let walker = WalkDir::new(root).into_iter();
     let mut result: Vec<(String, Metadata)> = Vec::new();
 
