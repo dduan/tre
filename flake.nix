c511d35c48844afcd1b728d2db0d8993ef646f35
Daniel Duan <daniel@duan.ca>
2022-03-13 15:57:17 -0700

    Embed nix build logic
    ... instead of importing from nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,11 +8,26 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    rec {
-      packages = flake-utils.lib.flattenTree {
-        tre-command = pkgs.tre-command;
-      };
-      defaultPackage = packages.tre-command;
-    });
+      with nixpkgs.legacyPackages.${system};
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          tre-command = rustPlatform.buildRustPackage rec {
+            pname = "tre-command";
+            version = "0.3.6";
+            src = ./.;
+            cargoSha256 = "1f7yhnbgccqmz8hpc1xdv97j53far6d5p5gqvq6xxaqq9irf9bgj";
+            lockFile = ./Cargo.lock;
+            nativeBuildInputs = [ installShellFiles ];
+            preFixup = ''
+              installManPage manual/tre.1
+            '';
+          };
+        };
+        defaultPackage = packages.tre-command;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust-analyzer
+          ];
+        };
+      });
 }
