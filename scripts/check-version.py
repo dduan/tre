ba76b35fe73f6f814764667fc9103a4597f6bdab
Daniel Duan <daniel@duan.ca>
2022-03-13 16:20:13 -0700

    Check flake version against everything else

--- scripts/check-version.py
+++ scripts/check-version.py
@@ -4,23 +4,14 @@ import re
 import sys
 
 expected = re.search(r'name = "tre-command"\nversion = "(.+)"', open('Cargo.toml').read(), re.M).group(1)
-cargo_lock = re.search(r'name = "tre-command"\nversion = "(.+)"', open('Cargo.lock').read(), re.M).group(1)
-changelog = re.search(r'# main\s+#\s*(.+)', open('CHANGELOG.md').read(), re.M).group(1)
+versions = {}
+versions['Cargo.lock'] = re.search(r'name = "tre-command"\nversion = "(.+)"', open('Cargo.lock').read(), re.M).group(1)
+versions['CHANGELOG.md'] = re.search(r'# main\s+#\s*(.+)', open('CHANGELOG.md').read(), re.M).group(1)
+versions['flake.nix'] = re.search(r'version = "(.+)"', open('flake.nix').read(), re.M).group(1)
 manual_match = re.search(r'.TH "TRE" "1" "2020-08-05" "TRE (\d+)\\&.(\d+)\\&.(\d+)" "Tre Manual"', open('manual/tre.1').read())
-manual = "{}.{}.{}".format(manual_match.group(1), manual_match.group(2), manual_match.group(3))
+versions['manual/tre.1'] = "{}.{}.{}".format(manual_match.group(1), manual_match.group(2), manual_match.group(3))
 
-has_error = False
-if expected != cargo_lock:
-    print("version mismatch: expected {}; found {} in Cargo.lock.".format(expected, cargo_lock), file=sys.stderr)
-    has_error = True
-
-if expected != changelog:
-    print("version mismatch: expected {}; found {} in CHANGELOG.md.".format(expected, cargo_lock), file=sys.stderr)
-    has_error = True
-
-if expected != manual:
-    print("version mismatch: expected {}; found {} in manual/tre.1.".format(expected, cargo_lock), file=sys.stderr)
-    has_error = True
-
-if has_error:
-    exit(1)
+for file in versions:
+    if expected != versions[file]:
+        print("version mismatch: expected {expeted}; found {versions[file]} in {file}", file=sys.stderr)
+        exit(1)
