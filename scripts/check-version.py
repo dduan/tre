c7cd7fb10b98f6a96f022569730b24b256bd7a3d
Daniel Duan <daniel@duan.ca>
2022-06-19 00:42:27 -0700

    Bump version to 0.4.0 (#113)

--- scripts/check-version.py
+++ scripts/check-version.py
@@ -7,7 +7,7 @@ expected = re.search(r'name = "tre-command"\nversion = "(.+)"', open('Cargo.toml
 versions = {}
 versions['Cargo.lock'] = re.search(r'name = "tre-command"\nversion = "(.+)"', open('Cargo.lock').read(), re.M).group(1)
 versions['CHANGELOG.md'] = re.search(r'#\s*([0-9.]+)', open('CHANGELOG.md').read(), re.M).group(1)
-manual_match = re.search(r'.TH "TRE" "1" "2020-08-05" "TRE (\d+)\\&.(\d+)\\&.(\d+)" "Tre Manual"', open('manual/tre.1').read())
+manual_match = re.search(r'.TH "TRE" "1" "[0-9-]+" "TRE (\d+)\\&.(\d+)\\&.(\d+)" "Tre Manual"', open('manual/tre.1').read())
 versions['manual/tre.1'] = "{}.{}.{}".format(manual_match.group(1), manual_match.group(2), manual_match.group(3))
 
 for file in versions:
